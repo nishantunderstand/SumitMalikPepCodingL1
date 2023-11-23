875a4f298202d35b7a1abd2138fff742449ca9e7
Nishant Raj <nishant.read@gmail.com>
2023-11-23 20:44:13 +0530

    Updated_By_Lenovo-bat File

--- src/a0a_CodingBasic/FindSquareRootByIteratingNumber.java
+++ src/a0a_CodingBasic/FindSquareRootByIteratingNumber.java
@@ -2,7 +2,7 @@ package a0a_CodingBasic;
 
 public class FindSquareRootByIteratingNumber {
 	public static void main(String[] args) {
-		int num = 29;
+		int num = 37;
 		int i = 1;
 		while (i * i <= num) {
 			i++;
