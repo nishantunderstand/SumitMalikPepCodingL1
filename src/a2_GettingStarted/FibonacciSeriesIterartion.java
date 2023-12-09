8ff1ea8d27f6f98f5d6044e3730497c78557ce7b
Nishant Raj <nishant.read@gmail.com>
2023-12-09 20:42:25 +0530

    LenovoBat(Nov-2023)

--- src/a2_GettingStarted/FibonacciSeriesIterartion.java
+++ src/a2_GettingStarted/FibonacciSeriesIterartion.java
@@ -5,7 +5,7 @@ public class FibonacciSeriesIterartion {
 		int n1 = 0;
 		int n2 = 1;
 		for (int i = 0; i < 10; i++) {
-			System.out.print(n1 + " ");
+			System.out.print(n1 + " "); // Here we are printing 1st Number
 			int n3 = n1 + n2;
 			n1 = n2;
 			n2 = n3;
