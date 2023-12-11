5fe7941e2e2c7ab85c7860d3dcfece1cf456aef5
Nishant Raj <nishant.read@gmail.com>
2023-12-11 22:47:10 +0530

    LenovoBat

--- src/a2_GettingStarted/PrimeFactorization.java
+++ src/a2_GettingStarted/PrimeFactorization.java
@@ -3,11 +3,12 @@ package a2_GettingStarted;
 public class PrimeFactorization {
 
 	public static void main(String[] args) {
-		int n = 46;
+		int n = 0;
+		// n=46;
 		n = 100;
-		n = 17;
-		n = 24;
-		n = 1440;
+		// n = 17;
+		// n = 24;
+		// n = 1440;
 		// n = 1;
 		for (int div = 2; div <= n; div++) {
 			while (n % div == 0) {
--- src/a2_GettingStarted/_Pratice.java
+++ src/a2_GettingStarted/_Pratice.java
@@ -2,29 +2,20 @@ package a2_GettingStarted;
 
 public class _Pratice {
 	public static void main(String[] args) {
-		BreakWhile(5);
-		BreakFor(5);
+		primeFactor(11);
 	}
 
-	private static void BreakWhile(int n) {
-		int i = 0;
-		while (i < n) {
-			System.out.println("While" + i);
-			if (i == 4) {
-				break;
-			}
-			i++;
+	private static void primeFactor(int n1) {
+		int n = n1;
+		// Program Failing Condition
+		if (n <= 0 || n == 1) {
+			System.out.println("Try Postive Number and Number Greater than 1");
 		}
-	}
-
-	private static void BreakFor(int n1) {
-		int i = 0;
-		for (; i < n1; i++) {
-			System.out.println("For" + i);
-			if (i == 2) {
-				System.exit(0);
+		// Program Passing Condition
+		for (int div = 2; div < n; div++) {
+			if (n % div == 0) {
+				System.out.println(div);
 			}
 		}
-
 	}
 }
