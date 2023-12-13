224dacb69300a73260ab2df870f6f9e1f72815b9
Nishant Raj <nishant.read@gmail.com>
2023-12-13 22:05:27 +0530

    LenovoBat

--- src/a2_GettingStarted/_Pratice.java
+++ src/a2_GettingStarted/_Pratice.java
@@ -2,19 +2,28 @@ package a2_GettingStarted;
 
 public class _Pratice {
 	public static void main(String[] args) {
-		primeFactor(11);
+		primeFactor(100);
 	}
 
 	private static void primeFactor(int n1) {
 		int n = n1;
+
 		// Program Failing Condition
-		if (n <= 0 || n == 1) {
-			System.out.println("Try Postive Number and Number Greater than 1");
+		if (n <= 0) {
+			System.out.println("Try Positive Number ");
+			return; // Exit the method if the condition is not met
 		}
+
+		if (n == 1) {
+			System.out.println("Try Number Greater than 1");
+			return; // Exit the method if the condition is not met
+		}
+
 		// Program Passing Condition
-		for (int div = 2; div < n; div++) {
-			if (n % div == 0) {
-				System.out.println(div);
+		for (int div = 2; div <= n; div++) {
+			while (n % div == 0) {
+				n = n / div;
+				System.out.println(div + " ");
 			}
 		}
 	}
