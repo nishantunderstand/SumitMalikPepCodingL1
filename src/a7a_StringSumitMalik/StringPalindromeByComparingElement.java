77b3ebc6d49b2de41b130f3820df7da3cdaaca1a
Nishant Raj <nishant.read@gmail.com>
2024-05-17 20:31:20 +0530

    LenovoBat

--- src/a7a_StringSumitMalik/StringPalindromeByComparingElement.java
+++ src/a7a_StringSumitMalik/StringPalindromeByComparingElement.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class StringPalindromeByComparingElement {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Enter Your String :");
