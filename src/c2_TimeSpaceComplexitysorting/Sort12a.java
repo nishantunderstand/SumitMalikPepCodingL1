789e0681dd7b760aedd3770c50b4318ba2fdb14d
Nishant Raj <nishant.read@gmail.com>
2024-06-12 22:09:54 +0530

    LenovoBat 12-Jun-24 22:09:53.95

--- src/c2_TimeSpaceComplexitysorting/Sort12a.java
+++ src/c2_TimeSpaceComplexitysorting/Sort12a.java
@@ -3,7 +3,7 @@ package c2_TimeSpaceComplexitysorting;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Sort012 {
+public class Sort12a {
 	public static void swap(int[] arr, int i, int j) {
 		System.out.println("Swapping index " + i + " and index " + j);
 		int temp = arr[i];
@@ -14,37 +14,34 @@ public class Sort012 {
 	public static void main(String[] args) {
 		System.out.println("Sorting 0-1-2:");
 		Scanner sc = new Scanner(System.in);
-		/*
-		 * int n = sc.nextInt();
-		 * int arr[] = new int[n];
-		 * for (int i = 0; i < arr.length; i++) {
-		 * arr[i] = sc.nextInt();
-		 * }
-		 */
+
 		int[] arr = new int []{1,1,2,2,0,0,};
 		System.out.println("Arrays:" + Arrays.toString(arr));
-		Sort012(arr);
+		Sort012a(arr);
 		System.out.println("Arrays:" + Arrays.toString(arr));
 
 		sc.close();
 	}
 
-	private static void Sort012(int[] arr) {
+	private static void Sort012a(int[] arr) {
 		int i = 0;
 		int j = 0;
 		int k = arr.length - 1;
 		while (i <= k) {
-			if (arr[i] == 1) {
-				i++;
-			} else if (arr[i] == 0) {
+			 if (arr[i] == 0) {
 				swap(arr, i, j);
 				i++;
 				j++;
-			} else {
+			} 
+			else if (arr[i] == 1) {
+				i++;
+			}
+			else if (arr[i]==2){
 				swap(arr, i, k);
 				k--;
 			}
 		}
 
 	}
+
 }
